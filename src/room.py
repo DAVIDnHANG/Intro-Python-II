b83b27e6bbe61722c2390510aede1a880a1e460c
DAVID HANG <davidnhang@gmail.com>
2020-03-12 16:13:25 -0700

    Add files via upload

--- src/room.py
+++ src/room.py
@@ -8,4 +8,17 @@ class Room():
         self.list_items=[]
 
 
+    def add_item(self, item):
+        self.item_items.append(item)
+
+    def print_items(self):
+        output = ""
+        if len(self.list_items)==0:
+            return None
+        else:
+            for item in self.list_items:
+                output += f"{item.name}"
+
+        return output
+
 
