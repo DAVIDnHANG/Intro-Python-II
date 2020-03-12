2649b3732966dffb67c424722609f10bc7178a79
DAVID HANG <davidnhang@gmail.com>
2020-03-12 03:21:10 -0700

    update adv,player,room

--- src/adv.py
+++ src/adv.py
@@ -38,7 +38,8 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+def player(self):
+    init.
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +50,5 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+
--- src/room.py
+++ src/room.py
@@ -1,2 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room():
+    def __init__(self, Player, description):
+        self.Player = Player
+        self.description = description
+        self.list_items=[]
+
+
+
